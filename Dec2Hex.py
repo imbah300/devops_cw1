9daa1c7f9c89f306dfe59abe36334abe8273d2a0
imbah300 <imbah300@caledonian.ac.uk>
2026-03-11 10:41:23 +0000

    final commit

--- Dec2Hex.py
+++ Dec2Hex.py
@@ -16,14 +16,16 @@ def decimal_to_hex(decimal_value):
     print(f"Hexadecimal representation is: {hexadecimal}")
     return hexadecimal  # Return the hexadecimal value for testing
 
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <decimal_number>")
+        return
+    try:
+        decimal_value = int(sys.argv[1])
+        decimal_to_hex(decimal_value)
+    except ValueError:
+        print("Please provide a valid integer.")
+
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        try:
-            decimal_value = int(sys.argv[1])
-            decimal_to_hex(decimal_value)
-        except ValueError:
-            print("Please provide a valid integer.")
-    else:
-        print("Usage: python script.py <decimal_number>")
-
+    main()
